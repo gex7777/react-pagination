3653a17db70b2bf3922452edf6ad46f6c96e7218
George <gex7777@gmail.com>
2023-03-22 01:21:54 +0530

    create interfaces for fetching API data

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface ApiResponse {
+  users: User[];
+}
+
+export interface User {
+  ID: string;
+  JobTitle: string;
+  EmailAddress: string;
+  FirstNameLastName: string;
+  Email: string;
+  Phone: string;
+  Company: string;
+}
